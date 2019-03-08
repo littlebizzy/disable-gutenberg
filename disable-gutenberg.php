1586c92c7d840110342f05492369f91156df0030
Pau <pauiglesias@gmail.com>
2019-03-08 11:13:34 +0100

    Updates for PBP 1.2.0 implementation.

--- disable-gutenberg.php
+++ disable-gutenberg.php
@@ -3,43 +3,26 @@
 Plugin Name: Disable Gutenberg
 Plugin URI: https://www.littlebizzy.com/plugins/disable-gutenberg
 Description: Completely disables the Gutenberg block editor and enables the classic WordPress post editor (TinyMCE aka WYSIWYG) for lighter coding and simplicity.
-Version: 1.0.0
+Version: 1.1.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
+PBP Version: 1.2.0
 WC requires at least: 3.3
-WC tested up to: 3.4
+WC tested up to: 3.5
 Prefix: DSBGTB
 */
 
 // Plugin namespace
 namespace LittleBizzy\DisableGutenberg;
 
-// Aliased namespaces
-use LittleBizzy\DisableGutenberg\Notices;
-
-// Block direct calls
-if (!function_exists('add_action')) {
-	die;
-}
-
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = 'dsbgtb';
-const VERSION = '1.0.0';
+const VERSION = '1.1.0';
+const REPO = 'littlebizzy/disable-gutenberg';
 
-// Loader
-require_once dirname(FILE).'/helpers/loader.php';
-
-// Admin Notices
-// Notices\Admin_Notices::instance(FILE);
-
-/**
- * Admin Notices Multisite check
- * Uncomment "return;" to disable this plugin on Multisite installs
- */
-if (false !== Notices\Admin_Notices_MS::instance(FILE)) { /* return; */ }
-
-// Run the main class
-Helpers\Runner::start('Core\Core', 'instance');
+// Boot
+require_once dirname(FILE).'/helpers/boot.php';
+Helpers\Boot::instance(FILE);
